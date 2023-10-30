5fad1c138fd8be528e00f268e046eefdda1edb0d
Abid <abid1702@outlook.com>
2023-10-30 12:29:46 +0000

    nearly finished code

--- bundle.js
+++ bundle.js
@@ -1,23 +1,37 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const abid = require("./script2")
-require("./script3")
+require("./script2")
 
-console.log(abid)
+console.log("Hello from scriptjs")
 
-console.log("Hello Reddy from default script")
+const fruitForm = document.querySelector("#inputSection form")
+fruitForm.addEventListener("submit", extractFruit)
 
-const fruitForm = document.querySelector("#inputSection form");
-console.log(fruitForm)
+function extractFruit(e) {
+    e.preventDefault()
+    console.log(e.target[0])
+    console.log(e.target.fruitInput.value) //console.log(e.target[0].value)
+    addFruit(e.target[0].value)
+    console.log(e.target[0].value = "") //clearing the form
+}
+
+const fruitList = document.querySelector("#fruitSection ul")
+function addFruit(fruit) {
+    if (fruit === "") {
+        console.log("Invalid Fruit")
+    } else {
+        const li = document.createElement("li")
+        li.textContent = fruit
+        fruitList.appendChild(li)
+    }
+}
 
 
 
-},{"./script2":2,"./script3":3}],2:[function(require,module,exports){
+},{"./script2":2}],2:[function(require,module,exports){
 const x = "abid"
 
 console.log("hello Script2")
 
 
 module.exports = x
-},{}],3:[function(require,module,exports){
-console.log("Hello Script3")
 },{}]},{},[1]);
--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const abid = require("./script2")
-require("./script3")
+require("./script2")
 
-console.log(abid)
+console.log("Hello from scriptjs")
 
-console.log("Hello Reddy from default script")
+const fruitForm = document.querySelector("#inputSection form")
+fruitForm.addEventListener("submit", extractFruit)
 
-const fruitForm = document.querySelector("#inputSection form");
-console.log(fruitForm)
+function extractFruit(e) {
+    e.preventDefault()
+    console.log(e.target[0])
+    console.log(e.target.fruitInput.value) //console.log(e.target[0].value)
+    addFruit(e.target[0].value)
+    console.log(e.target[0].value = "") //clearing the form
+}
+
+const fruitList = document.querySelector("#fruitSection ul")
+function addFruit(fruit) {
+    if (fruit === "") {
+        console.log("Invalid Fruit")
+    } else {
+        const li = document.createElement("li")
+        li.textContent = fruit
+        fruitList.appendChild(li)
+    }
+}
 
 
